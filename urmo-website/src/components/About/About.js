943ff6e8b16ba50659646a78f40b12c0a73d4ebd
kachow4 <kchow1sfo@gmail.com>
2022-06-09 10:52:11 -0700

    about page

--- urmo-website/src/components/About/About.js
+++ urmo-website/src/components/About/About.js
@@ -2,6 +2,9 @@ import React from 'react'
 import Navigation from '../Navigation/Navigation';
 import Carousel from 'react-bootstrap/Carousel';
 import HeaderImage from "../../assets/images/10thGenWinterFlagpoles.jpg";
+import WorkshopImage from "../../assets/images/9thGenAuditionWorkshop.PNG";
+import AuditionsImage from "../../assets/images/Auditions.PNG";
+import InterviewsImage from "../../assets/images/10thGenPreviewNight.jpg";
 import '../../assets/css/About/About.css';
 
 export default function About() {
@@ -22,7 +25,10 @@ export default function About() {
                 <section id="mission">
                     <h3 className='section-title'>Our Mission</h3>
                     <div className='mission-statement'>
-                        Unique Representation of Movement (URMO) is a family dedicated to growth and individual expression through movement. Our goal is to uphold the history of dance culture and our team’s passion for artistry in an inclusive space to foster the purest representation of ourselves.
+                        Unique Representation of Movement (URMO) is a family dedicated to growth<br></br>
+                        and individual expression through movement. Our goal is to uphold the <br></br>
+                        history of dance culture and our team’s passion for artistry in an inclusive<br></br> 
+                        space to foster the purest representation of ourselves.
                     </div>
                 </section>
                 <section id="performances">
@@ -66,13 +72,37 @@ export default function About() {
                     <div className='audition-content'>
                         <div className='column'>
                             Audition Workshops
+                            <img className="column-img" src={WorkshopImage} alt="9th Gen Audition Workshop #1"/>
+                            <div className='description'>
+                                Workshops are held during Week 7 of Fall Quarter and  
+                                taught by our three coordinators. Workshop location
+                                is usually at the ARC Outdoor basketball 
+                                courts, but will be TBD. These workshops are free!
+                            </div>
                         </div>
                         <div className='column'>
                             Audition Day
+                            <img className="column-img" src={AuditionsImage} alt="9th Gen Audition Workshop #1"/>
+                            <div className='description'>
+                                Auditions will be held the Thursday of Week 7 at
+                                Student Center Terrace. After turning in your audition 
+                                application and audition fee, we will ask auditonees 
+                                to perform the three audition pieces in groups of threes 
+                                onstage in front of our judging panel. After all the rounds 
+                                are complete, we will hold callbacks for certain groups. 
+                                Afterwards, there will be an informal and optional cypher 
+                                for auditonees to freestyle and show us any other tricks they may have! 
+                            </div>
                         </div>
                         <div className='column'>
                             Interview
-                            Workshops will 
+                            <img className="column-img" src={InterviewsImage} alt="9th Gen Audition Workshop #1"/>
+                            <div className="description">
+                                Once auditions are over, board will deliberate and reach out to auditionees
+                                to move on to the interview round through phone call or text. URMO board will 
+                                let you know the time and location of your interview. Once all interviews have
+                                been conducted, the roster should be released by late Saturday afternoon!
+                            </div>
                         </div>
                     </div>
                 </section>
--- urmo-website/src/components/Navigation/Navigation.js
+++ urmo-website/src/components/Navigation/Navigation.js
@@ -1,15 +1,9 @@
 import React from 'react';
-<<<<<<< HEAD
 import { Link } from 'react-router-dom';
 import '../../assets/css/Navigation/Navigation.css'
 import {ReactComponent as URMOLogo} from '../../assets/images/URMOlogo.svg';
 
 
-=======
-import '../../assets/css/Navigation/Navigation.css'
-import {ReactComponent as URMOLogo} from '../../assets/images/URMOlogo.svg';
-
->>>>>>> e85fb4f0017ee9878f3114e17d751275b24b35b2
 export default function Navigation() {
   return (
     <div id="nav-container">
@@ -19,7 +13,6 @@ export default function Navigation() {
         />
       </div>
 
-<<<<<<< HEAD
     <nav id="nav-tabs">
         <Link to="/" className='tab'>Home</Link>
         <Link to="/about" className='tab'>About</Link>
@@ -27,16 +20,6 @@ export default function Navigation() {
         <Link to="/team" className='tab'>Our Team</Link>
         <Link to="/resources" className='tab'>Resources</Link>
       </nav>
-=======
-      <div id="nav-tabs">
-        <a href=".">About</a>
-        <a href=".">History</a>
-        <a href=".">Our Team</a>
-        <a href=".">Resources</a>
-
-      </div>
-
->>>>>>> e85fb4f0017ee9878f3114e17d751275b24b35b2
     </div>
   )
 }
